a1b9fa4aede623a02a98c8a37dea5ce16dfd76d7
Fely <40307073+jfelya@users.noreply.github.com>
2020-02-09 20:48:01 -0400

    Delete conexion.php

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";
-$username = "guayuco";
-$password = "digital";
-$db_name = "guayuco_digital";
-$conexion = new mysqli($host, $username, $password, $db_name) or die("La conexión falló");
-?>
