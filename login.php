8417653fc83b45a090228de6237ad824dfd86d2f
jfelya <fely756@gmail.com>
2020-02-12 18:52:38 -0400

    reestructured css in login

--- login.php
+++ login.php
@@ -19,28 +19,47 @@ sesionActiva();
 </head>
 <body>
 	<div class="container-fluid">
-			<p class="title">Bienvenido a <br>Equipate Full</p><br>
-			<form action="procesos/login_proceso.php" method="post">
-				<div class="form-group row">
-					<label class="col-sm-4 col-form-label" for="usuario">Usuario
-					</label>
-					<div class="col-sm-7">
-						<input class="form-control" id="usuario" type="text" name="usuario" autofocus="yes">
-					</div>
+		<div class="row">
+			
+			<div class="col-sm-12">
+				<h1 class="title">Bienvenido a Equipate Full</h1>
+			</div>
+		</div><br><br>
+		<form action="procesos/login_proceso.php" method="post">
+			<div class="form-group row">
+				<div class="col-sm-3"></div>
+				<label class="col-sm-3 col-form-label" for="usuario">Usuario
+				</label>
+				<div class="col-sm-3">
+					<input class="form-control" id="usuario" type="text" name="usuario" autofocus="yes">
 				</div>
-				<div class="form-group row">
-					<label class="col-sm-4 col-form-label" for="contrasena">Contraseña
-					</label>
-					<div class="col-sm-7">
-						<input class="form-control" id="contrasena" type="password" name="contrasena">
-					</div>
+				<div class="col-sm-3"></div>
+			</div>
+			<div class="form-group row">
+				<div class="col-sm-3"></div>
+				<label class="col-sm-3 col-form-label" for="contrasena">Contraseña
+				</label>
+				<div class="col-sm-3">
+					<input class="form-control" id="contrasena" type="password" name="contrasena">
 				</div>
-				<a class="enlaces" href="recuperar_contrasena.php">Olvidé mi contraseña</a> /
-				<a class="enlaces" href="registrarse.php">Registrarse</a><br><br>
-				<center>
+				<div class="col-sm-3"></div>
+			</div><br>
+			<div class="row">
+				<div class="col-sm-3"></div>
+				<div class="col-sm-6">
+					<a class="enlaces" href="recuperar_contrasena.php">Olvidé mi contraseña</a> /
+					<a class="enlaces" href="registrarse.php">Registrarse</a>
+				</div>
+				<div class="col-sm-3"></div>
+			</div><br><br>
+			<div class="row">
+				<div class="col-sm-4"></div>
+				<div class="col-sm-4">
 					<input class="btn btn-outline-info btn-block" type="submit" name="ingresarBtn" value="Ingresar">
-				</center>
-			</form>
+				</div>
+				<div class="col-sm-4"></div>
+			</div>
+		</form>
 	</div>
 </body>
 </html>
